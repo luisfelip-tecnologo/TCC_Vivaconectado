3e0ffb227986cbd505b9cc7815619da8ffc0ac2b
luisfelip-tecnologo <luis.felipe04450@gmail.com>
2026-04-15 09:48:27 -0300

    Create cadastro.cc

---
+++ TCC_Site/public/css/cadastro.cc
@@ -0,0 +1,14 @@
+body {
+  background: #f0f4f8;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  min-height: 100vh;
+}
+
+.cadastro-box {
+  background: #fff;
+  padding: 35px;
+  border-radius: 20px;
+}
+</style>
