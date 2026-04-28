cd411fb532109c85179654f587b4570d6c9f285c
luisfelip-tecnologo <luis.felipe04450@gmail.com>
2026-04-27 21:27:10 -0300

    Update script.js

--- TCC_Site/public/main.js/script.js
+++ TCC_Site/public/main.js/script.js
@@ -1,31 +1,58 @@
 window.addEventListener("componentesCarregados", function(){
 
-const iconePerfil = document.getElementById("iconePerfil");
+const areaPerfil = document.querySelector(".header-perfil");
+
+if(!areaPerfil) return;
+
+const usuarioLogado = localStorage.getItem("usuarioLogado");
+const nomeUsuario = localStorage.getItem("nomeUsuario") || "Usuário";
+
+if(usuarioLogado){
+
+areaPerfil.innerHTML = `
+<div class="perfil-dropdown">
+
+<span class="bem-vindo">
+Bem-vindo, ${nomeUsuario} 👋
+</span>
+
+<i class="bi bi-person-circle perfil-icone"
+id="botaoPerfil"></i>
+
+<div class="dropdown-conteudo" id="menuPerfil">
+<a href="Perfil.html">Meu Perfil</a>
+<a href="#" id="btnSair">Sair da Conta</a>
+</div>
+
+</div>
+`;
+
+const botaoPerfil = document.getElementById("botaoPerfil");
 const menuPerfil = document.getElementById("menuPerfil");
 const btnSair = document.getElementById("btnSair");
 
-if(!iconePerfil) return;
-
-iconePerfil.addEventListener("click", function(){
-
-   const usuarioLogado = localStorage.getItem("usuarioLogado");
-
-   if(usuarioLogado){
-      menuPerfil.classList.toggle("fixo");
-   } else {
-      window.location.href = "login.html";
-   }
-
+botaoPerfil.addEventListener("click", function(){
+menuPerfil.classList.toggle("fixo");
 });
 
-if(btnSair){
 btnSair.addEventListener("click", function(e){
-   e.preventDefault();
+e.preventDefault();
 
-   localStorage.removeItem("usuarioLogado");
+localStorage.removeItem("usuarioLogado");
+localStorage.removeItem("nomeUsuario");
 
-   window.location.href="login.html";
+window.location.href="login.html";
 });
+
+}
+else{
+
+areaPerfil.innerHTML = `
+<a href="Login.html" class="login-icone">
+<i class="bi bi-person perfil-icone"></i>
+</a>
+`;
+
 }
 
 });
